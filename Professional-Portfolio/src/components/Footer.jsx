f7e9338abcd1b88166233cfc15220ee422c54dc0
Michael Scott <mscotteagle21@gmail.com>
2024-05-17 18:45:24 -0500

    added info and structure for the aboutMe page, Contact, and Footer

---
+++ Professional-Portfolio/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/Rashuneagle">GitHub</a>
+      <a href="https://linkedin.com/in/michael-scott-59575b13a">LinkedIn</a>
+      <a href="https://stackoverflow.com/users/22988282/michael-scott">Stack Overflow</a>
+    </footer>
+  );
+};
+
+export default Footer;
