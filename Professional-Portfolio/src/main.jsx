d9bd5e6af9dbbc16fefe74176d3f3e331b5a6699
Michael Scott <mscotteagle21@gmail.com>
2024-05-23 18:21:30 -0500

    fixed main.jsx to be able to deploy app

--- Professional-Portfolio/src/main.jsx
+++ Professional-Portfolio/src/main.jsx
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
